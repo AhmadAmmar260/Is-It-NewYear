67850be480bb5fafffc1147d28e1a401a324fd24
Ahmed Ammar <AhmadAljawahery@gmail.com>
2021-06-23 10:25:28 +0300

    Complete views work

--- NewYear/Year/views.py
+++ NewYear/Year/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from datetime import date 
 
 def check(request):
-    current_day = date.today().day
-    current_month = date.today().month
+    current_day = int(date.today().day)
+    current_month = int(date.today().month)
+    if current_month == 1 and current_day == 1:
+        return render(request,"check.html",{'checking':'YES',})
+    else:
+        return render(request,"check.html",{'checking':'NO',})
+        
 
-    return render(request,"check.html")
+    
